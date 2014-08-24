4dd2a2fb1a68dad8649e28a310ebe907585f9f42
thefringeninja <joao.paul@braganca.name>
2014-08-24 17:23:33 +0200

    basic session details

---
+++ src/BuildStuff14/Model/SessionDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BuildStuff14.Model
+{
+    public class SessionDetail
+    {
+        public SessionDetail(Speaker speaker, string title, string details, DateTime startingOn, TimeSpan? length = null)
+        {
+            Speaker = speaker;
+            Title = title;
+            Details = details;
+        }
+
+        public Speaker Speaker { get; private set; }
+        public string Details { get; private set; }
+        public string Title { get; private set; }
+    }
+}
---
+++ src/BuildStuff14/Views/SessionDetailPage.cs
@@ -0,0 +1,65 @@
+﻿using BuildStuff14.Model;
+using Xamarin.Forms;
+
+namespace BuildStuff14.Views
+{
+    public class SessionDetailPage : ContentPage
+    {
+        public SessionDetailPage(SessionDetail sessionDetail)
+        {
+            #region Initialize some properties on the Page
+
+            Padding = new Thickness(20);
+            BindingContext = sessionDetail;
+
+            #endregion
+
+            var speaker = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                Children =
+                {
+                    new Image
+                    {
+                        HeightRequest = 200,
+                        Source = ImageSource.FromFile(sessionDetail.Speaker.ImageUri),
+                    },
+                    new Label
+                    {
+                        Text = sessionDetail.Speaker.DisplayName
+                    }
+                }
+            };
+
+            var title = new Label {Text = sessionDetail.Title};
+
+            var headerLayout = new Grid
+            {
+                ColumnDefinitions =
+                {
+                    new ColumnDefinition {Width = GridLength.Auto},
+                    new ColumnDefinition {Width = new GridLength(1, GridUnitType.Star)},
+                },
+                Children =
+                {
+                    {speaker, 0, 0},
+                    {title, 1, 0}
+                }
+            };
+
+            var details = new Label
+            {
+                Text = sessionDetail.Details
+            };
+            Content = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                Children =
+                {
+                    headerLayout,
+                    details
+                }
+            };
+        }
+    }
+}
